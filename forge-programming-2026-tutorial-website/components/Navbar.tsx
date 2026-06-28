c661cacbf5db071b7cdfe58c5087726060b0516d
Piaoyi Wu <piaoyiwu1@gmail.com>
2026-06-27 22:24:30 -0600

    starting on some of the pages. nav animation is same as ones ive done in the past but altered to match the theme. pages are home, code, tools, and electrical in that order

--- forge-programming-2026-tutorial-website/components/Navbar.tsx
+++ forge-programming-2026-tutorial-website/components/Navbar.tsx
@@ -2,13 +2,21 @@ import Link from "next/link";
 // use link not <a> so the entire website doesnt get re renedered every tiome someone clicks a navlink
 
 export default function Navbar() {
+    // big notes
+    // had to add flex to div otherwise gap-4 dont work
+    // link classname is for hover (same one i used in the past but changed a bit to fit the theme!)
+    const fatNavLinkAnimationClass = "relative after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-black after:transition-all after:duration-250 hover:after:w-full"
     return (
-        <nav className="flex p-4 border-b shadow-md">
-            <div className="ml-auto flex gap-4 pr-10">
-            <Link href="/">HOME</Link>
-            <Link href="/code">CODE</Link>
-            <Link href="/tools">TOOLS</Link>
-            <Link href="/electrical">ELECTRICAL</Link>
+        <nav className="flex border-b shadow-md">
+            <div className="flex pl-10 p-3.5 font-bold">
+                <h1>4421 FORGE</h1>
+            </div>
+            <div className="ml-auto flex gap-5 pr-10 p-4">
+
+            <Link className={fatNavLinkAnimationClass} href="/">HOME</Link>
+            <Link className={fatNavLinkAnimationClass} href="/code">CODE</Link>
+            <Link className={fatNavLinkAnimationClass} href="/tools">TOOLS</Link>
+            <Link className={fatNavLinkAnimationClass} href="/electrical">ELECTRICAL</Link>
 
             </div>
         </nav>
