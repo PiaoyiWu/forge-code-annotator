c08041529de5aadc66877b0993bd03ba4849b908
Piaoyi Wu <piaoyiwu1@gmail.com>
2026-06-29 21:40:01 -0600

    added line number changes to code/page.tsx

--- forge-code-annotator/app/code/page.tsx
+++ forge-code-annotator/app/code/page.tsx
@@ -1,7 +1,8 @@
 import fs from "fs";
 import path from "path";
-import { codeToHtml } from "shiki";
+import { codeToHtml, codeToHast } from "shiki";
 import FolderBar from "@/components/FolderBar";
+import {toHtml} from "hast-util-to-html";
 
 export default async function CodePage() {
 
@@ -11,7 +12,15 @@ export default async function CodePage() {
     const dispCode = fs.readFileSync(codePath, "utf-8");
 
     const addTextColors = await codeToHtml(dispCode, {lang: "java", theme: "one-light"});
+    const createHast = await codeToHast(dispCode,
+        {lang: "java", theme: "one-light"}
+    );
 
+    const preTag = createHast.children[0] as any;
+    const codeTag = preTag.children[0] as any;
+    const lines = codeTag.children.filter((node:any) => (node.type === "element" && node.tagName === "span")) as any[];
+
+    // code was written after FilePage (/[filepage]/page.tsx)
     return (
         <main className="flex flex-col h-screen max-h-screen overflow-hidden">
             <h1 className="font-bold p-4 text-5xl font-mono ml-5 flex-none">CODE</h1>
@@ -20,7 +29,20 @@ export default async function CodePage() {
                     <FolderBar />
                 </div>
                 
-                <div className="h-full overflow-y-auto border-l border-black bg-[#FAFAFA] text-xs p-10 min-w-0 flex-1 overflow-x-auto [&_pre]:w-max [&_pre]:min-w-full" dangerouslySetInnerHTML={{ __html: addTextColors }} />
+                <div className="h-full overflow-y-auto border-l border-black bg-[#FAFAFA] text-xs p-10 min-w-0 flex-1 overflow-x-auto [&_pre]:w-max [&_pre]:min-w-full">
+                    {lines.map((line, i) => {
+                        const lineNumber = i + 1;
+                        const lineToHTML = toHtml(line);
+
+                        return (
+                            <div key={lineNumber} data-linenumber={lineNumber} className="flex whitespace-pre">
+                                <span className="text-gray-500 pr-2 min-w-8 select-none">{lineNumber}</span>
+                                <span dangerouslySetInnerHTML={{__html: lineToHTML}} />
+
+                            </div>
+                        );
+                    })}
+                </div>
 
             </div>
         </main>
